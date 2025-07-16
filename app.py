b8c6383d310994f1b56c9f3c3a3987da520ab6df
nitin negi <taradevi246810@gmail.com>
2025-07-16 13:38:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config(page_title="Eye Disease Detector", layout="centered")
 from tensorflow.keras.models import load_model
 from PIL import Image, ImageOps
 import numpy as np
